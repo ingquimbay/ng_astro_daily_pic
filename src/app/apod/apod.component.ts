28bbbe092070e4a81b32db03cc5095a621896510
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-11-30 12:28:54 -0600

    Pass the current date to the API

--- src/app/apod/apod.component.ts
+++ src/app/apod/apod.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 import { ApodService } from "../api/apod.service";
 import { Apod } from "../models/apod";
 
