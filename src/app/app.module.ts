593570b79c6f12c345b4fa27c3e419e6c9a598c9
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-11-30 17:44:19 -0600

    added hash strategy to routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from "@angular/common/http";
+import { LocationStrategy, HashLocationStrategy } from "@angular/common";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { NgApodConfig } from "../../config/ng_apod.config";
     HttpClientModule,
     SafePipeModule
   ],
-  providers: [NgApodConfig
+  providers: [NgApodConfig,
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ],
   bootstrap: [AppComponent]
 })
